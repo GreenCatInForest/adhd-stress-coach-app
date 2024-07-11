edd455fd9637decbfa9ef135c3a084e8d5152169
yanakozarenko2022 <yanakozarenko2024@gmail.com>
2024-07-11 16:10:00 +0100

    styled navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,6 +5,12 @@ export const Navbar = () => {
         <div className="navbar-main">
             <h1>ADHD  CH{']'}</h1>
             <img src={nightModeIcon} alt="Night Mode Icon" className="night-mode-icon"/>
+            <ul class="navbar-main-links-wrapper">
+                <li class="navbar-main-link">what is it</li>
+                <li class="navbar-main-link">how works</li>
+                <li class="navbar-main-link">what for</li>
+                <li class="navbar-main-link">who</li>
+            </ul>     
         </div>
     )
 }
