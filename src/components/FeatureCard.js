bbbd2a86d1beafa53afa29d9ca1447280dbc277b
yanakozarenko2022 <yanakozarenko2024@gmail.com>
2024-07-13 15:59:56 +0100

    masonry sort of

--- src/components/FeatureCard.js
+++ src/components/FeatureCard.js
@@ -1,16 +1,19 @@
-export const FeatureCard = ({feature_card_title, submenus}) => {
+export const FeatureCard = ({featureCardData}) => {
 
     return (
             <div className="feature-card-main">
-                {feature_card_title&&<h3>{feature_card_title}</h3>}
-                {submenus&&submenus.map((submenu) => {
+                {featureCardData.feature_card_title&&<h3>{featureCardData.feature_card_title}</h3>}
+                {featureCardData.submenus&&featureCardData.submenus.map((submenu) => {
                     return (
+                        <>
                         <div className="submenu-main">
                             <h4>{submenu.submenu_title}</h4>
                             <p>{submenu.description}</p>
                         </div>
+                        </>
                     )
                 })}
+                <button>Go</button>
             </div>
     )
 }
--- src/components/FeatureContainer.js
+++ src/components/FeatureContainer.js
@@ -10,12 +10,33 @@ let featureCardDatas = [
         description: "This is a feature card",
         submenus: [
             {
-                submenu_title: "Submenu 1",
+                submenu_title: "Breath 4-4-4-4 for 3 times",
                 submenu_description: "This is a submenu"
             },
             {
-                submenu_title: "Submenu 2",
+                submenu_title: "Listen relax noices for 30 seconds",
+                submenu_description: "This relaxation is a submenu",
+                submenu_options: ['sea waves', 'rain', 'forest', 'birds', 'mantra'],
+            },
+            {
+                submenu_title: "Mussle progressive relaxation",
                 submenu_description: "This is a submenu"
+            }
+        ],
+        buttonText: "Click me"
+    },
+    {
+        feature_card_title: "Plan",
+        description: "This is a feature card",
+        submenus: [
+            {
+                submenu_title: "Write down 3 things you want to achieve today",
+                submenu_description: "This is a submenu"
+            },
+            {
+                submenu_title: "Listen relax noices for 30 seconds",
+                submenu_description: "This relaxation is a submenu",
+                submenu_options: ['sea waves', 'rain', 'forest', 'birds', 'mantra'],
             },
             {
                 submenu_title: "Submenu 3",
@@ -25,23 +46,65 @@ let featureCardDatas = [
         buttonText: "Click me"
     },
     {
-        feature_card_title: "Plan",
-        description: "This is a feature card",
-        buttonText: "Click me"
-    },
-    {
         feature_card_title: "Focus on",
         description: "This is a feature card",
-        buttonText: "Click me"
+        submenus: [
+            {
+                submenu_title: "Write down what are you doing right now",
+                submenu_description: "This is a submenu"
+            },
+            {
+                submenu_title: "Concentrate on your task for 30 seconds and then take a break 5 minutes",
+                submenu_description: "This relaxation is a submenu",
+                submenu_options: [],
+            },
+            {
+                submenu_title: "Submenu 3",
+                submenu_description: "This is a submenu"
+            }
+        ],
     },
     {
         feature_card_title: "Achieve",
         description: "This is a feature card",
+        submenus: [
+            {
+                submenu_title: "Write down 1 small thing you want to achieve this week and why",
+                submenu_description: "This is a submenu"
+            },
+            {
+                submenu_title: "Write down 1 small thing you want to achieve today and why",
+                submenu_description: "This relaxation is a submenu",
+                submenu_options: ['sea waves', 'rain', 'forest', 'birds', 'mantra'],
+            },
+            {
+                submenu_title: "Write here what inspires you",
+                submenu_description: "This is a submenu"
+            },
+            {
+                submenu_title: "List what you want to achieve. Why? Who will benefit from it? Why it's important for you?",
+                submenu_description: "This is a submenu"
+            }
+        ],
         buttonText: "Click me"
     },
     {
         feature_card_title: "Journal",
         description: "This is a feature card",
+        submenus: [
+            {
+                submenu_title: "Write here a 10 gratitudes what for are you grateful when wake up",
+                submenu_description: "This is a submenu"
+            },
+            {
+                submenu_title: "Write here a 10 gratitudes what for are you grateful when go to bed",
+                submenu_description: "This relaxation is a submenu",
+            },
+            {
+                submenu_title: "Submenu 3",
+                submenu_description: "This is a submenu"
+            }
+        ],
         buttonText: "Click me"
     },
     {
@@ -71,13 +134,16 @@ let featureCardDatas = [
         <div className="feature-container-main">
             {featureCardDatas.map((featureCardData) => {
                 return (
+                    <>
                 <FeatureCard 
-                feature_card_title = {featureCardData.feature_card_title} 
-                description = {featureCardData.description} 
-                buttonText = {featureCardData.buttonText}
+                featureCardData = {featureCardData}
                 key = {featureCardData.feature_card_title} 
-                submenus = {featureCardData.submenus}/>)
+              />
+                </>
+            )
+                
             })}
+                
         </div>
     )
 }
